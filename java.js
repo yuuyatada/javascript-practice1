e74ae5be398d211382aacd30204325d5db67aa62
EC2 Default User <c134141@gmail.com>
2023-04-07 06:27:08 +0000

    配列から条件を満たすときの要素を取得したい

--- java.js
+++ java.js
@@ -83,26 +83,65 @@
  //});
  
  //API等から出力するユーザーデータの配列
- const userList = [
-     {id: 1,name: '田中',address:'田中'},
-     {id: 2,name: '鈴木',address:'宮城'},
-     {id: 3,name: '高橋',address:'福岡'}
-     ];
+ //const userList = [
+     //{id: 1,name: '田中',address:'田中'},
+     //{id: 2,name: '鈴木',address:'宮城'},
+    // {id: 3,name: '高橋',address:'福岡'}
+     //];
      
      //コンテナー
      
-     const container = document.querySelector('.container');
+     //const container = document.querySelector('.container');
      
      //userListの配列の各要素についてのループ
-     userList.forEach((userDate) => {
+     //userList.forEach((userDate) => {
          //各要素を書き足す
-         container.innerHTML += `
-         <div class="card">
-         <h2>${userDate.name}</h2>
-         <p>出身地:${userDate.address}</p>
-         </div>
-         `;
+         //container.innerHTML += `
+        // <div class="card">
+         //<h2>${userDate.name}</h2>
+         //<p>出身地:${userDate.address}</p>
+         //</div>
+         //`;
+     //});
+     
+     
+     
+     //idキーとnameキーを持つユーザーデータの配列
+     const userDataList = [
+         {id :123 , name: '高橋'},
+         {id :1021 , name: '鈴木'},
+         {id :6021 , name: '後藤'}
+         ];
+         
+      //**検索IDを入力するinput要素
+     const searchIdInput = document.querySelector('#search-id-input');
+     
+     //** 検索結果を表示する要素
+     const searchResult = document.querySelector('#search-result');
+     
+     //文字が入力されるたびにチェックを行う
+     searchIdInput.addEventListener('keyup',() => {
+         //検索IDを取得する
+         const searchId = Number(event.target.value);
+         findUser(searchId);
      });
+     
+     //*ユーザーを検索する
+     function findUser(searchId) {
+         //該当データを取得する
+         const targetData =userDataList.find((data) =>
+         data.id === searchId);
+     
+     //該当データが存在しなかったら「該当者なし」と表示して終了
+     if (targetData == null){
+         searchResult.textContent = '該当者なし';
+         return;
+     }
+     
+     //該当データの名前を表示する
+     searchResult.textContent = targetData.name;
+     }
+         
  
 
 
