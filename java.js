ead2f22a6aadfb31ce1d9a76b7355d347831b868
EC2 Default User <c134141@gmail.com>
2023-04-11 05:01:11 +0000

    ボタンを押した際にイベントを発生させる

--- java.js
+++ java.js
@@ -235,31 +235,38 @@
                 //document.querySelector('.user_list').innerHTML = listHtml;
            // }
            
-           setInterval(() => {
+           //setInterval(() => {
                //現在時間を取得
-               const now = new Date();
+             //  const now = new Date();
                //時間の数値を取得
-               const h = now.getHours();//時間(0~23)
-               const m = now.getMinutes();//分(0~59)
-               const s = now.getSeconds();//秒(0~59)
+               //const h = now.getHours();//時間(0~23)
+               //const m = now.getMinutes();//分(0~59)
+               //const s = now.getSeconds();//秒(0~59)
                
                //針の角度に反映する
                //短針
-               const degH = h * (360/12) + m *(360/12/60);
+               //const degH = h * (360/12) + m *(360/12/60);
                //分針
-               const degM = m * (360/12);
+               //const degM = m * (360/12);
                //秒針
-               const degS = s * (360/12);
+               //const degS = s * (360/12);
                
-               const elementH = document.querySelector('.lineHour');
-               const elementM = document.querySelector('.lineMin');
-               const elementS = document.querySelector('.lineSec');
+               //const elementH = document.querySelector('.lineHour');
+               //const elementM = document.querySelector('.lineMin');
+               //const elementS = document.querySelector('.lineSec');
                
-               elementH.style.transform = `rotate(${degH}deg`;
-               elementM.style.transform = `rotate(${degM}deg`;
-               elementS.style.transform = `rotate(${degS}deg`;
-           },50);
+               //elementH.style.transform = `rotate(${degH}deg`;
+               //elementM.style.transform = `rotate(${degM}deg`;
+               //elementS.style.transform = `rotate(${degS}deg`;
+           //},50);
             
+               //ユーザーの操作に合わせてイベントを起こす
+               const button = document.querySelector('.button');
+               button.addEventListener('click', onClickButton);
+               
+               function onClickButton(){
+                console.log('クリックされました');
+               }
                
                
            
