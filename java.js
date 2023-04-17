7c953214ef22b39ea8052b85d68026d097b4f723
EC2 Default User <c134141@gmail.com>
2023-04-17 05:46:53 +0000

    ボックスをクリックしたらコメントを表示させる

--- java.js
+++ java.js
@@ -405,7 +405,16 @@
         //logElement.innerHTML = 'こんにちは';
         
         //foo要素
-        const element = document.getElementById('foo');
+        //const element = document.getElementById('foo');
+        
+        //各.box要素に対してループ
+        document.querySelectorAll('.box').forEach((targetBox) => {
+         //.box要素をクリックしたときの処理
+         targetBox.addEventListener('click',() => {
+          //クリックされた.box要素のテキストを表示
+          alert(`${targetBox.textContent}がクリックされました`);
+         });
+        });
  
 
 
