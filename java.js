98eaf059a4372ba1c441bbacc0f144e4cde058d4
EC2 Default User <c134141@gmail.com>
2023-04-19 06:14:02 +0000

    要素を受動的に削除したい

--- java.js
+++ java.js
@@ -432,16 +432,24 @@
        //container.appendChild(myBox);
        //},3000);
        
-       const container = document.querySelector('.container');
+       //const container = document.querySelector('.container');
        //挿入する.new-box要素
-       const newBox = `<div class="new-box box">.new-box要素</div>`;
+       //const newBox = `<div class="new-box box">.new-box要素</div>`;
        
-       setTimeout(() => {
+      // setTimeout(() => {
         //container要素内先頭に.new-box要素を追加する
-        container.insertAdjacentHTML('afterbegin',newBox);
+       // container.insertAdjacentHTML('afterbegin',newBox);
         //.container要素の直後に.new-box要素を追加する
-        container.insertAdjacentHTML('afterend',newBox);
-       },3000);
+       // container.insertAdjacentHTML('afterend',newBox);
+      // },3000);
+      
+      //3秒後に処理を行う
+      setTimeout(() => {
+       const parentElement = document.querySelector('#parent');
+       const childElenment = document.querySelector('#child');
+       //#child要素を取り除く
+       parentElement.removeChild(childElenment);
+      },3000);
  
 
 
