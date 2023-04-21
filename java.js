80fbb99312ab7c43addb72ab90124f0d0b624096
EC2 Default User <c134141@gmail.com>
2023-04-21 06:33:49 +0000

    テキストエリア変更を検知する

--- java.js
+++ java.js
@@ -523,11 +523,24 @@
   //}
   
   //textareaの参照
-  const element = document.querySelector('#myText');
+  //const element = document.querySelector('#myText');
   //値
-  const value = element.value;
-  console.log(value);
+  //const value = element.value;
+ // console.log(value);
   //結果：今日の天気は（改行）曇りです
+  
+  //textareaの参照
+  const element = document.querySelector('#myText');
+  //イベントを登録
+  element.addEventListener('input',handleChange);
+  
+  function handleChange(event){
+   //値を取得
+   const value = event.target.value;
+   //改行コードを改行タグに変換
+   const htmlStr = value.split('/n').join('<br />');
+   document.querySelector('.log').innerHTML = htmlStr
+  }
    
    
 
