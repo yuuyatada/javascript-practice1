33c153f3c063801b4cd73845408556d14bbede28
EC2 Default User <c134141@gmail.com>
2023-04-24 13:31:05 +0000

    ドロップダウンの情報を取得したい

--- java.js
+++ java.js
@@ -567,17 +567,25 @@
  //});
  
  //input要素を参照
- const element = document.querySelector('#myFile');
+ //const element = document.querySelector('#myFile');
  
  //ファイル選択ダイアログが選択されたら
- element.addEventListener('change', (event) => {
-  const target = event.target;
+ //element.addEventListener('change', (event) => {
+  //const target = event.target;
   //選択されたファイルを参照
-  const files = target.files;
+  //const files = target.files;
   //配列になってるので、０ばんめのファイルを参照
-  const file = files[0];
+  //const file = files[0];
   
   //ユーザーが選択したファイル名を表示
-  alert(`${file.name}が選択されました`);
- });
+  //alert(`${file.name}が選択されました`);
+// });
+
+//select要素の参照
+const element = document.querySelector('#mySelect');
+//値を取得
+const value = element.value;
+//整形して画面に表示
+const log = `表示されているのは${value}です`;
+document.querySelector('.log').innerHTML = log;
 
