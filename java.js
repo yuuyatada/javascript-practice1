cb47fbdbf4eec19ab4a67cf544a0c61bcb0f6b34
EC2 Default User <c134141@gmail.com>
2023-04-26 12:50:02 +0000

    カラーピッカーの変更を周知したい

--- java.js
+++ java.js
@@ -597,13 +597,19 @@
 //document.querySelector('.log').innerHTML = `現在の値は${value}です`;
 
 //input要素の参照
-const element = document.querySelector('#myRnage');
+//const element = document.querySelector('#myRnage');
 //変更イベントを監視
-element.addEventListener('input',handleChange);
-function handleChange(event){
+//element.addEventListener('input',handleChange);
+//function handleChange(event){
  //現在の値を取得
- const value =event.target.value;
+ //const value =event.target.value;
  //画面に表示
- document.querySelector('.log').innerHTML = `現在の値は${value}です`;
-}
+ //document.querySelector('.log').innerHTML = `現在の値は${value}です`;
+//}
+
+const element = document.querySelector('#myColor');
+const value = element.value;
+console.log(value);
+
+
 
