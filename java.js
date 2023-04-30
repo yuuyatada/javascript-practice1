aeedcd3fc35620dbec98399d413c24c36bdc2d4e
EC2 Default User <c134141@gmail.com>
2023-04-30 12:59:39 +0000

    SVGで動的に追加したい

--- java.js
+++ java.js
@@ -611,10 +611,17 @@
 //const value = element.value;
 //console.log(value);
 
-const element = document.querySelector('.rect');
-element.addEventListener('transitionend',(event) => {
+//const element = document.querySelector('.rect');
+//element.addEventListener('transitionend',(event) => {
  //アニメーション完了時のコード
-});
+//});
 
-
+const myCircle = document.createElementNS(
+ 'http://www.w3.org/2000/svg','circle');
+ myCircle.setAttribute('cx','100');
+ myCircle.setAttribute('cy','100');
+ myCircle.setAttribute('r','100');
+ myCircle.setAttribute('fill','#FFFF8D');
+ const mySvg = document.querySelector('#mySvg');
+ mySvg.appendChild(myCircle);
 
