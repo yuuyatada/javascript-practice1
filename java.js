0050b493f40af71358916cce87647a38729d8956
EC2 Default User <c134141@gmail.com>
2023-05-01 14:44:16 +0000

    キャンバス要素に塗りと線を描く

--- java.js
+++ java.js
@@ -644,7 +644,14 @@
 // }
 //}
 
+//const canvas = document.querySelector('#my-canvas');
+//const context = canvas.getContext('2d');
+//context.fillRect(0,0,100,100);
+
 const canvas = document.querySelector('#my-canvas');
 const context = canvas.getContext('2d');
+context.fillStyle = 'red';
 context.fillRect(0,0,100,100);
+
+
 
